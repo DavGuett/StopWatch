6f7cffe2a32cf7aa40cbf58d32219f8ba6fe4668
DavGuett <soulsec@protonmail.com>
2022-10-27 20:46:46 -0300

    primeiro commit

---
+++ StopWatch/Program.cs
@@ -0,0 +1,12 @@
+﻿static void Start()
+{
+    int time = 10;
+    int currentTime = 0;
+
+    while (currentTime != time)
+    {
+        Console.WriteLine(currentTime);
+        currentTime++;
+    }
+}
+
