885e80da055b25ed796a69b5f13896505a901548
DavGuett <soulsec@protonmail.com>
2022-10-29 08:15:06 -0300

    menu finalizado

--- StopWatch/Program.cs
+++ StopWatch/Program.cs
@@ -1,4 +1,21 @@
-﻿static void Start(int time)
+﻿
+using System.Runtime.Intrinsics.Arm;
+
+static void Menu()
+{
+    Console.Clear();
+    Console.WriteLine("S - Segundo");
+    Console.WriteLine("M - Minuto");
+    Console.WriteLine("0 - Sair");
+    Console.WriteLine("Quanto tempo deseja contar?");
+
+    string data = Console.ReadLine().ToLower();
+    char type = char.Parse(data.Substring(data.Length - 1, 1)) ;
+    int time = int.Parse(data.Substring(0, data.Length - 1));
+    Console.WriteLine(time);
+}
+
+static void Start(int time)
 {
     int currentTime = 0;
 
@@ -9,8 +26,12 @@
         Console.WriteLine(currentTime);
         Thread.Sleep(1000);
     }
+
+    Console.Clear();
+    Console.WriteLine("Cronômetro finalizado, retornando para o menu");
+    Thread.Sleep(2500);
 }
 
 
-Start(5);
+Menu();
 
