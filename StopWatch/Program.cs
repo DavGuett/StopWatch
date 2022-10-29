e63f926073717a52d975054b27bba63dedbe3b23
DavGuett <soulsec@protonmail.com>
2022-10-29 08:22:47 -0300

    cronômetro finalizado

--- StopWatch/Program.cs
+++ StopWatch/Program.cs
@@ -12,7 +12,17 @@ static void Menu()
     string data = Console.ReadLine().ToLower();
     char type = char.Parse(data.Substring(data.Length - 1, 1)) ;
     int time = int.Parse(data.Substring(0, data.Length - 1));
-    Console.WriteLine(time);
+    int multiplier = 1;
+    if (type == 'm')
+    {
+        multiplier = 60;
+    }
+
+    if (time == 0)
+    {
+        System.Environment.Exit(0);
+    }
+    Start(time * multiplier);
 }
 
 static void Start(int time)
@@ -30,6 +40,7 @@ static void Start(int time)
     Console.Clear();
     Console.WriteLine("Cronômetro finalizado, retornando para o menu");
     Thread.Sleep(2500);
+    Menu();
 }
 
 
